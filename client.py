c02340144720c8b42b6c771cbb8fd3a5969b5442
nicolasgula273910 <ng273910@fi365.ort.edu.uy>
2026-05-25 21:23:18 -0300

    add server and client

---
+++ client.py
@@ -0,0 +1,11 @@
+import asyncio
+from fastmcp import Client
+
+async def main():
+    client = Client("server.py")
+
+    async with client
+        result = await client.call_tool("say_hello", {"nombre":"Nicolas"})
+        print(result)
+
+asyncio.run(main())
--- server.py
+++ server.py
@@ -5,4 +5,7 @@ mcp = FastMCP("HelloServer")
 @mcp.tool
 def say_hello(nombre: str) -> str:
     """Multiplies two numbers together."""
-    return "Hi, how are you AI engineer?"
+    return f"Hi {nombre} how are you AI engineer?"
+
+if __name__ == "__main__":
+    mcp.run()
