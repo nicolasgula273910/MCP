d11bb3833a29c4261571876949c9d4e18ad269c0
nicolasgula273910 <ng273910@fi365.ort.edu.uy>
2026-05-25 21:07:44 -0300

    add function in server

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("MyServer")
+mcp = FastMCP("HelloServer")
+
+@mcp.tool
+def say_hello(nombre: str) -> str:
+    """Multiplies two numbers together."""
+    return "Hi, how are you AI engineer?"
